0329504cd49c5d6ec1b54381fe639191c2866d16
Amarante <amarantegba@gmail.com>
2022-02-27 13:12:18 -0300

    Reformulacao da pasta

---
+++ ORIENTACAO_A_OBJETOS_COM_LINGUAGEM_C_SHARP/Capitulo02/PrimeiroProjeto/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using PrimeiroProjeto;
+
+Instituicao instituicao = new Instituicao();
+System.Console.Write("Informe o NOME da instituição : ");
+instituicao.Nome = System.Console.ReadLine();
+System.Console.Write("Informe o ENDEREÇO da instituição : ");
+instituicao.Endereco = System.Console.ReadLine();
+System.Console.WriteLine("====================================");
+System.Console.WriteLine($"Obrigado por informar os dados para a  {instituicao.Nome}");
+System.Console.WriteLine("Pressione qualquer tecla para encerrar.");
+System.Console.ReadKey();
+
